d9a1ba4338ee26a7241311fe2c7638eacf85c803
Konstantin <knetsov91@gmail.com>
2024-12-01 09:33:27 +0200

    add topic and email notification

--- lib/aws-exam-retake-stack.ts
+++ lib/aws-exam-retake-stack.ts
@@ -1,4 +1,5 @@
 import * as cdk from 'aws-cdk-lib';
+import { Subscription, SubscriptionProtocol, Topic } from 'aws-cdk-lib/aws-sns';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
@@ -6,6 +7,14 @@ export class AwsExamRetakeStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-   
+    const thresholdTopic  = new Topic(this, 'ThresholdTopic', {
+      topicName: "thresholdTopic"
+    })
+
+    new Subscription(this, 'ErrorSubscription', {
+      topic: thresholdTopic,
+      protocol: SubscriptionProtocol.EMAIL,
+      endpoint: 'email'
+    })
   }
 }
