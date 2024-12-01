a78426f26b725292bb5e1861aff33461105547b3
Konstantin <knetsov91@gmail.com>
2024-12-01 12:00:48 +0200

    Add functionality for getting item from DynamoDb and return it to user.Correct status code when request is not valid

--- src/queryFunction.ts
+++ src/queryFunction.ts
@@ -1,16 +1,47 @@
 import { DynamoDBClient, GetItemCommand } from '@aws-sdk/client-dynamodb';
+ 
 
 const dynamoDbClient = new DynamoDBClient({});
 export const handler = async (event: any) => {
     const tableName = process.env.TABLE_NAME;
-    const topicArn = process.env.TOPIC_ARN;
-
-   
     console.log(event);
     
- 
+    const body = JSON.parse(event.body);
+    if (body.productId){
+        
+        let productId = body.productId;
+        console.log(productId);
+        const response = await dynamoDbClient.send(new GetItemCommand({
+            Key: {
+                "productId": {
+                    "S": productId
+                }
+            },
+            TableName: tableName
+        }))
+        if (!('Item' in response)) {
+            return {
+                statusCode: 404,
+                body: "No such product"
+            }
+        } 
+        let responseObj = {
+            
+            "productId": response.Item!.productId.S,
+            "shortDescription": response.Item!.shortDescription.S,
+            "tag":  response.Item!.tag.S,
+            "cost": response.Item!.cost.N
+            
+        }
+        console.log(responseObj);
+        return {
+            statusCode: 200,
+            "item": JSON.stringify(responseObj)
+        }
+    }
+
     return {
-        statusCode: 200,
-        body: "response"
+        statusCode: 400,
+        body: "Bad request"
     }
 }
